5e2ca74ad5055d5ceeb6846aaeb1afdd6fb0d8e8
Anurag Basu <AnuragBasu@gmail.com>
2023-11-23 13:46:37 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function add()
+{
+  var num1, num2, sum;
+  num1 = parseInt(document.getElementById("Number1").value);
+  num2 = parseInt(document.getElementById("Number2").value);
+  sum = num1 + num2;
+  document.getElementById("Sum").innerHTML = sum;
+} 
+
+
+// console.log("Sum of given numbers is :", sum);
+
+
+
+
